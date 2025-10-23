49ec42f1da53789941e3f4fce9ac4f5f4aa3c59a
Maksym Soiko <somax2003@gmail.com>
2025-10-23 13:52:16 +0300

    first commit

---
+++ shop/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+	products = Product.objects.all().order_by('-created_at')
+	return render(request, 'main/product-list.html', {'products': products})
